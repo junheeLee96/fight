42c607ea120aa933aa813835a16f64f283f9754a
junheeLee96 <dlwnsgml203@naver.com>
2023-03-12 01:50:33 +0900

    start page

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,25 +1,95 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from "react";
+
+const canvas_width: number = 1200;
+const canvas_height: number = 800;
+
+const margin_top: number = 50;
+
+const arrow_width: number = 50;
+const arrow_height: number = 50;
+
+type arrow = {
+  x: number;
+  y: number;
+};
 
 const Canvas = () => {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
-    const canvasRef = useRef<HTMLCanvasElement>(null)
-    // const contextRef = useRef<CanvasRenderingContext2D | undefined>(null)
+  const [canvas, setCnavas] = useState<HTMLCanvasElement | null>(null);
+  const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
+  const [arrow, setArrow] = useState<arrow>({
+    x: 0,
+    y: 0,
+  });
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        if (!canvas) {
-            return;
-        }
-        canvas.width = 1400;
-        canvas.height = 800;
-    },[])
+  const Draw_arrow = () => {
+    if (ctx) {
+      ctx.beginPath();
+      ctx.fillStyle = "red";
+      ctx.moveTo(arrow.x, arrow.y);
+      ctx.lineTo(arrow.x, arrow.y + arrow_height);
+      ctx.lineTo(arrow.x + arrow_width, arrow.y + arrow_height / 2);
+      ctx.fill();
+      setTimeout(() => {
+        ctx.save();
+        ctx.clearRect(
+          arrow.x,
+          arrow.y,
+          arrow.x + arrow_width,
+          arrow.y + arrow_height
+        );
+        setTimeout(() => {
+          ctx.restore();
+          requestAnimationFrame(Draw_arrow);
+        }, 500);
+      }, 500);
+    }
+  };
 
+  useEffect(() => {
+    // console.log(canvas);
+    // Arrow(1, 2);
+    Draw_arrow();
+    // requestAnimationFrame(Draw_arrow);
+  }, [arrow]);
 
-    return (
-        <canvas ref={canvasRef}>
-            
-        </canvas>
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (!canvas || !ctx) {
+      return;
+    }
+    setArrow({
+      x: canvas_width / 2 - margin_top - 100,
+      y: canvas_height / 2 - margin_top - 40,
+    });
+
+    setCtx(ctx);
+    setCnavas(canvas);
+
+    canvas.width = canvas_width;
+    canvas.height = canvas_height;
+    ctx.strokeRect(0, 0, canvas_width, canvas_height);
+
+    ctx.font = "48px serif";
+    ctx.fillText(
+      "Start",
+      canvas_width / 2 - margin_top,
+      canvas_height / 2 - margin_top
     );
+    ctx.fillText(
+      "About",
+      canvas_width / 2 - margin_top,
+      canvas_height / 2 - margin_top * -1
+    );
+
+    // ctx.fillRect(0, 0, canvas_width / 2 - 50, canvas_height);
+    // ctx.fill();
+
+    // ctx.arc(50, 50, 10, Math.PI * 2, true);
+  }, []);
+  return <canvas ref={canvasRef}></canvas>;
 };
 
 export default Canvas;
